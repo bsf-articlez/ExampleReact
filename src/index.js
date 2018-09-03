b0156a1704c79f1bdbcb1ed264d66513fdbda944
bsf-articlez <bsf_articlez@outlook.com>
2018-09-03 10:53:37 +0700

    [Add] redux create store ,add and subtract state

--- src/index.js
+++ src/index.js
@@ -1,8 +1,38 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+//import React from 'react';
+//import ReactDOM from 'react-dom';
+//import App from './App';
+import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(
+
+import {createStore} from 'redux';
+
+const reducer = (state,action) => {
+    switch(action.type){
+        case "ADD":
+            state+=action.payload;
+            break;
+        case "SUBTRACT":
+            state-=action.payload;
+            break;
+    }
+    return state;
+}
+const store=createStore(reducer, 15000);
+
+store.subscribe(()=>{
+    console.log("Update store:", store.getState());
+})
+store.dispatch({
+    type:"ADD",
+    payload:500
+})
+store.dispatch({
+    type:"SUBTRACT",
+    payload:1000
+})
+/*ReactDOM.render(
     <App />, 
     document.getElementById('root')
-);
+);*/
+registerServiceWorker();
+
